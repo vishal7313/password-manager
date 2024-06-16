b51480895d5f3350f222f5955ac6b97fb38bc583
Vishal <vishal.ranjan7777@gmail.com>
2024-06-16 11:41:14 -0500

    Front end for the password manager

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 const Navbar = () => {
   return (
-    <nav className='bg-purple-200 flex justify-between items-center px-4 h-14'>
-        <div className="logo font-bold">
-            PassOP
+    <nav className='bg-slate-800 text-white'>
+      <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+        <div className="logo font-bold text-2xl">
+          <span className="text-green-700">&lt;</span>
+            Pass
+          <span className="text-green-700">OP/&gt;</span>
         </div>
         <ul>
             <li className="flex gap-4">
@@ -11,6 +14,7 @@ const Navbar = () => {
                 <a className='hover:font-bold' href="/">Contact</a>
             </li>
         </ul>
+      </div>
     </nav>
   )
 }
