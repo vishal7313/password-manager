2d349017cfe720691f865ebd7fb8d4f7508fe992
Vishal <vishal.ranjan7777@gmail.com>
2024-06-19 22:24:50 -0500

    Use `lordicon` icons for buttons and style the button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ const Navbar = () => {
     <nav className='bg-slate-800 text-white'>
       <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
         <div className="logo font-bold text-2xl">
-          <span className="text-green-700">&lt;</span>
+          <span className="text-green-500">&lt;</span>
             Pass
-          <span className="text-green-700">OP/&gt;</span>
+          <span className="text-green-500">OP/&gt;</span>
         </div>
         <ul>
             <li className="flex gap-4">
