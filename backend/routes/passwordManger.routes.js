980211caffce4113b39d2673600568b17e7a1182
Vishal <vishal.ranjan7777@gmail.com>
2024-06-26 23:20:02 -0500

    Add middle ware to check if a user is logged in or not before using the system

--- backend/routes/passwordManger.routes.js
+++ backend/routes/passwordManger.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { passwordManager } from '../controllers/passwordManager.controller.js';
+import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
-router.post('/password', passwordManager)
+router.post('/password', protectRoute, passwordManager)
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
 import passwordAuthRoutes from './routes/passwordManger.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/passwordAuth', passwordAuthRoutes);
 
